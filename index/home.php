3f5ba510c5d11310bfd688cf92741e85a43eb2ea
phamnga <phamnga.bibi89@gmail.com>
2021-12-20 22:03:22 +0700

    Nga commit file

---
+++ index/home.php
@@ -0,0 +1,153 @@
+<?php include('../template/header_main.php');?>
+<div class="contain-right">
+    <div class="control-page space">
+        <div class="control-left control-sty">
+            <i class="material-icon bi bi-arrow-left-short"></i>
+        </div>
+        <div class="control-right control-sty">
+            <i class="material-icon bi bi-arrow-right-short"></i>
+        </div>
+    </div>
+    <div class="header-material space">
+        <button class="nangcap vien">NÂNG CẤP</button>  
+        <div class=" dropdown space">
+            <i class="material-icon bi bi-person-circle"></i>
+            <a class="nav-linkk dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Phạm Thị Phương Nga</a>
+            <ul class="dropdown-menu mauchu"style="">
+                <li><a class="dropdown-item" href="#">Tài khoản</a></li>
+                <li><a class="dropdown-item" href="#">Hồ sơ</a></li>
+                <li><a class="dropdown-item" href="#">Nâng cấp lên Premium</a></li>
+                <li><a class="dropdown-item" href="#">Đăng xuất</a></li>
+            </ul>
+        </div>
+    </div>
+</div>  
+<div class="main">
+    <div class="main-inner-vien">
+        <div class="form-item text">
+            <div class="title-h space clear">
+                <h4 class=""style="font-weight:700; letter-spacing:-1.5px;">Chương trình đáng để thử</h4>
+                <a  class="see-more card-text-infor mauchu" href="">XEM TẤT CẢ</a>
+            </div>
+            <div class="clear"></div>
+            <div class="row ">
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="card-img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <span class="title-item text">Tri Kỷ Cảm Xúc</span>
+                                    <span class="card-text ">Web5Ngay</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="card-img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <span class="title-item text">Tri Kỷ Cảm Xúc</span>
+                                    <span class="card-text ">Web5Ngay</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="card-img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <span class="title-item text">Tri Kỷ Cảm Xúc</span>
+                                    <span class="card-text ">Web5Ngay</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="card-img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <span class="title-item text">Tri Kỷ Cảm Xúc</span>
+                                    <span class="card-text ">Web5Ngay</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="card-img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <span class="title-item text">Tri Kỷ Cảm Xúc</span>
+                                    <span class="card-text ">Web5Ngay</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="card-img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <span class="title-item text">Tri Kỷ Cảm Xúc</span>
+                                    <span class="card-text ">Web5Ngay</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>                
+        </div>
+        <div class="form-item text">
+            <div class="title-h space clear">
+                <h4 class=""style="font-weight:700; letter-spacing:-1.5px;">Bảng xếp hạng Nổi bật</h4>
+                <a  class="see-more card-text-infor mauchu" href="">XEM TẤT CẢ</a>
+            </div>
+            <div class="clear"></div>
+            <div class="row ">
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <p class="title-item text">Bài hát hàng đầu tại Toàn Cầu</p>
+                                    <p class="card-text-infor title-item">Thông tin cập nhật hàng tuần cập nhật tại</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-md-2 ">
+                    <div  class="box">
+                        <div class="cardd">
+                            <a href="">
+                                <img src="../img/anh-mang-dep-nhat-24.jpg" class="img-topp" alt="..."width="auto" height="155px">
+                                <div class="card-bodyy mauchu">
+                                    <p class="title-item text">Bài hát hàng đầu tại Toàn Cầu</p>
+                                    <p class="card-text-infor title-item">Thông tin cập nhật hàng tuần cập nhật tại</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>                
+        </div>
+    </div>
+</div> 
+<?php include('../template/footer-main.php');?>
